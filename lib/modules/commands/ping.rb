aa6cdf6e5655f3c2f641cf49fc2e5aec92197a79
z64 <zachnowicki@gmail.com>
2016-09-17 19:35:34 -0400

    enhance ping, set perm level

--- lib/modules/commands/ping.rb
+++ lib/modules/commands/ping.rb
@@ -4,8 +4,11 @@ module Bot
     # This used to check if bot is alive
     module Ping
       extend Discordrb::Commands::CommandContainer
-      command :ping do |event|
-        event << 'Pong!'
+      command(:ping,
+              description: 'checks if bot is alive',
+              usage: "#{BOT.prefix}ping",
+              permission_level: 4) do |event|
+        "`#{event.timestamp - Time.now} ms`"
       end
     end
   end
