29418dee3094ee52a654ae4dd2d00fd4d4660ffa
z64 <zachnowicki@gmail.com>
2016-09-17 23:07:12 -0400

    add message deletion

---
+++ lib/modules/commands/delete_message.rb
@@ -0,0 +1,29 @@
+module Powerbot
+  module DiscordCommands
+    # Deletes a message.
+    module DeleteMessage
+      extend Discordrb::Commands::CommandContainer
+      command(:delete_message,
+              required_permissions: [:manage_messages],
+              description: 'Deletes a message. Must be a moderator to use.',
+              usage: "#{BOT.prefix}delete_message message_id reason"
+      ) do |event, id, *reason|
+        reason = reason.join(' ')
+        message = Database::Message.find(message_id: id)
+        unless message.nil?
+          log = Database::ModLog.create(server_id: event.server.id,
+                                        action: 'Message Removal',
+                                        message: message,
+                                        reason: reason,
+                                        moderator_id: event.user.id,
+                                        offender_id: message.user_id)
+          log.post
+          event.message.delete
+          message.message.delete
+          return
+        end
+        'I couldn\'t find that message..'
+      end
+    end
+  end
+end
