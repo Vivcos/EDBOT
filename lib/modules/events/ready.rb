73e30383dde4e38378b04d2fe5aea401b2e5ab81
z64 <zachnowicki@gmail.com>
2016-09-17 23:44:24 -0400

    add comments and register nightly chat log dump

--- lib/modules/events/ready.rb
+++ lib/modules/events/ready.rb
@@ -4,10 +4,17 @@ module Powerbot
     module Ready
       extend Discordrb::EventContainer
       ready do |event|
+        # Configure bot
         event.bot.profile.avatar = File.open(CONFIG.avatar)
         event.bot.game = CONFIG.game
 
+        # Set owner permission level
         event.bot.set_user_permission(CONFIG.owner, 4)
+
+        # Register nightly chat log dump
+        SCHEDULER.cron '0 0 * * *' do
+          Database::Message.dump
+        end
       end
     end
   end
