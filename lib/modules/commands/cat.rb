9276202d07934fee759ed645f92f7ee51a69de5c
z64 <zachnowicki@gmail.com>
2016-09-17 21:54:34 -0400

    add cat

---
+++ lib/modules/commands/cat.rb
@@ -0,0 +1,16 @@
+module Powerbot
+  module DiscordCommands
+    # Fetches a random cat picture
+    module Cat
+      extend Discordrb::Commands::CommandContainer
+      bucket :cat, limit: 3, time_span: 60
+      command(:cat,
+              bucket: :cat,
+              rate_limit_message: 'You can summon more cats in %time%'\
+                                  ' seconds.',
+              help_available: false) do |event|
+        JSON.parse(RestClient.get('http://random.cat/meow'))['file']
+      end
+    end
+  end
+end
