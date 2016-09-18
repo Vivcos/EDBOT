1b378e3b899a24f1fff46d82b62c556d95161b7d
z64 <zachnowicki@gmail.com>
2016-09-17 23:09:47 -0400

    add min_args to delete_message

--- lib/modules/commands/delete_message.rb
+++ lib/modules/commands/delete_message.rb
@@ -5,6 +5,7 @@ module Powerbot
       extend Discordrb::Commands::CommandContainer
       command(:delete_message,
               required_permissions: [:manage_messages],
+              min_args: 1,
               description: 'Deletes a message. Must be a moderator to use.',
               usage: "#{BOT.prefix}delete_message message_id reason"
       ) do |event, id, *reason|
