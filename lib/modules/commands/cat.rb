088848aff756aeeed9a3e5a3c0c6ef9bcb8f384c
z64 <zachnowicki@gmail.com>
2016-09-18 00:24:30 -0400

    add channel limit to :cat

--- lib/modules/commands/cat.rb
+++ lib/modules/commands/cat.rb
@@ -9,7 +9,9 @@ module Powerbot
               rate_limit_message: 'You can summon more cats in %time%'\
                                   ' seconds.',
               help_available: false) do |event|
-        JSON.parse(RestClient.get('http://random.cat/meow'))['file']
+        unless event.channel.name == CONFIG.general_channel
+          JSON.parse(RestClient.get('http://random.cat/meow'))['file']
+        end
       end
     end
   end
