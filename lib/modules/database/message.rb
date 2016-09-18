90d53c56145e735298e54430bfd748b9b20ed00c
z64 <zachnowicki@gmail.com>
2016-09-17 20:50:15 -0400

    add method to fetch message from cache

--- lib/modules/database/message.rb
+++ lib/modules/database/message.rb
@@ -1,5 +1,6 @@
 module Powerbot
   module Database
+    # A Discord message
     class Message < Sequel::Model
       # Set timestamp before creation
       def before_create
@@ -10,6 +11,11 @@ module Powerbot
       # Log creation
       def after_create
         Discordrb::LOGGER.info "created message: #{inspect}"
+      end
+
+      # Fetch message from cache
+      def message
+        BOT.channel(channel_id).message(message_id)
       end
     end
   end
