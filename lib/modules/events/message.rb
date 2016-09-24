29c5bc8efc20c20e480e84912dc3790f21833402
z64 <zachnowicki@gmail.com>
2016-09-23 20:42:38 -0400

    prioritize other db actions over message logging to prevent lock

--- lib/modules/events/message.rb
+++ lib/modules/events/message.rb
@@ -8,17 +8,23 @@ module Powerbot
         server_name = event.server.nil? ? 'pm' : event.server.name
         attachment = event.message.attachments.first
         attachment_url = attachment.url unless attachment.nil?
-        Database::Message.create(
-          server_id: server_id,
-          server_name: server_name,
-          channel_id: event.channel.id,
-          channel_name: event.channel.name,
-          user_id: event.user.id,
-          user_name: event.user.distinct,
-          message_id: event.message.id,
-          message_content: event.message.content,
-          attachment_url: attachment_url
-        )
+        begin
+          Database::Message.create(
+            server_id: server_id,
+            server_name: server_name,
+            channel_id: event.channel.id,
+            channel_name: event.channel.name,
+            user_id: event.user.id,
+            user_name: event.user.distinct,
+            message_id: event.message.id,
+            message_content: event.message.content,
+            attachment_url: attachment_url
+          )
+        rescue
+          Discordrb::LOGGER.info 'database busy, waiting..'
+          sleep 1
+          retry
+        end
       end
     end
   end
