743520bc19146673e2435dada8e924e2414974ca
z64 <zachnowicki@gmail.com>
2016-10-04 00:40:20 -0400

    add method docs, logging, and composite for help entry

--- lib/modules/database/help_entry.rb
+++ lib/modules/database/help_entry.rb
@@ -2,9 +2,20 @@ module Powerbot
   module Database
     # A help entry.
     class HelpEntry < Sequel::Model
+      # Set up class on creation
       def before_create
         super
         self.timestamp ||= Time.now
+      end
+
+      # Log creation
+      def after_create
+        Discordrb::LOGGER.info "created help entry #{inspect}"
+      end
+
+      # Composite message for Discord
+      def composite
+        "🔰 `#{key}:#{id}` - #{text}"
       end
     end
   end
