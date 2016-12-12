ae549d82db9e14285f10d8a9b3d9006dfbba02e0
z64 <zachnowicki@gmail.com>
2016-12-12 02:51:11 -0500

    add :bubble command, some stylistic fixes

--- lib/modules/commands/traikoa.rb
+++ lib/modules/commands/traikoa.rb
@@ -8,8 +8,7 @@ module Powerbot
       command(:info,
               permission_level: 1,
               description: 'Displays info about a system',
-              usage: "#{BOT.prefix}info (system name)",
-              ) do |event, *name|
+              usage: "#{BOT.prefix}info (system name)") do |event, *name|
         name = name.join ' '
         sys = Powerbot::Traikoa::System.search(name).first
         next 'System not found..' unless sys
@@ -21,7 +20,7 @@ module Powerbot
       command(:distance,
               permission_level: 1,
               description: 'Get the distance between one or more systems',
-              usage: "#{BOT.prefix}distance system_a, system_b...",) do |event, *names|
+              usage: "#{BOT.prefix}distance system_a, system_b...") do |event, *names|
         names = names.join(' ').split(',').map(&:strip)
         next 'Please specify two or more systems.' unless names.count > 1
 
@@ -43,7 +42,7 @@ module Powerbot
       command(:route,
               permission_level: 1,
               description: 'Get the shortest-distance route between a collection of systems',
-              usage: "#{BOT.prefix}route system_a, system_b...",) do |event, *names|
+              usage: "#{BOT.prefix}route system_a, system_b...") do |event, *names|
         names = names.join(' ').split(',').map(&:strip)
         next 'Please specify three or more systems.' unless names.count > 2
 
@@ -63,6 +62,67 @@ module Powerbot
         nil
       end
 
+      command(:bubble,
+              permission_level: 3,
+              description: 'Gives you CC stats about a region of space',
+              usage: "#{BOT.prefix}bubble system name") do |event, *name|
+        name = name.join(' ')
+        sys = Powerbot::Traikoa::System.search(name).first
+        next 'System not found.' unless sys
+
+        bubble = sys.bubble
+
+        uncontrolled = bubble.select { |s| !s.exploitations.any? }
+        uncontrolled_cc = uncontrolled.map(&:cc_value).compact.reduce(:+)
+
+        contested = bubble.select(&:contested)
+        contested_cc = contested.map(&:cc_value).compact.reduce(:+)
+
+        controlled = bubble - contested - uncontrolled
+        controlled_cc = controlled.map(&:cc_value).compact.reduce(:+)
+
+        control_systems = Powerbot::Traikoa::ControlSystem.search(
+          bubble.map(&:exploitations).flatten.uniq
+        )
+
+        powers = Powerbot::Traikoa::API::Power.list.map { |p| p[:name] }
+
+        event.channel.send_embed do |e|
+          e.author = { name: 'Bubble Analysis', icon_url: "#{BOT.profile.avatar_url}" }
+
+          e.description = "**Uncontrolled:** #{uncontrolled_cc}\n"\
+                          "**Controlled:** #{controlled_cc}\n"\
+                          "**Contested:** #{contested_cc}"
+
+          control_systems.each do |cs|
+            systems = bubble.select { |s| s.exploitations.include? cs.id }
+            e.add_field(
+              name: "#{cs.name} (#{systems.map(&:cc_value).reduce(:+)}, #{powers[cs.power_id - 1]})",
+              value: systems.map do |s|
+                       s.exploitations.count > 1 ? "#{s.name} (#{s.cc_value})*" : "#{s.name} (#{s.cc_value})"
+                     end.join("\n"),
+              inline: true
+            )
+          end unless control_systems.empty?
+
+          e.add_field(
+            name: "Contested (#{contested_cc})",
+            value: contested.map do |s|
+                     "#{s.name} (#{s.cc_value})"
+                   end.join("\n")
+          ) unless contested.empty?
+
+          e.add_field(
+            name: "Uncontrolled (#{uncontrolled_cc})",
+            value: uncontrolled.map do |s|
+                     "#{s.name} (#{s.cc_value})"
+                   end.join("\n")
+          ) unless uncontrolled.empty?
+
+          e.footer = { text: '* - this system isn\'t contested, but is overlapped by other systems' }
+        end
+      end
+
       module_function
 
       def system_embed(sys)
@@ -76,14 +136,16 @@ module Powerbot
                  "Security: #{sys.security}\n"\
                  "Permit locked: #{sys.permit? ? 'Yes' : 'No'}\n"\
                  "CC value: #{sys.cc_value}\n"\
-                 "Contested: #{sys.contested? ? 'Yes' : 'No' }\n",
+                 "Contested: #{sys.contested? ? 'Yes' : 'No'}\n",
           inline: true
         )
-        e.add_field(
-          name: 'Stations',
-          value: sys.stations.map { |s| "[#{s[:pad_size]}] #{s[:name]}, #{s[:distance].nil? ? '?' : s[:distance]}ls #{s[:is_planetary] ? '(planetary)' : ''}" }.join("\n"),
-          inline: true
-        ) if sys.stations.any?
+        if sys.stations.any?
+          e.add_field(
+            name: 'Stations',
+            value: sys.stations.map { |s| "[#{s[:pad_size]}] #{s[:name]}, #{s[:distance].nil? ? '?' : s[:distance]}ls #{s[:is_planetary] ? '(planetary)' : ''}" }.join("\n"),
+            inline: true
+          )
+        end
         e.footer = { text: "id: #{sys.id} x: #{sys.x} y: #{sys.y} z: #{sys.z}" }
         e
       end
