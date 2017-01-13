c5211b4f20b285d4f90b16c5847e6a5b5a4a7837
z64 <zachnowicki@gmail.com>
2017-01-13 18:05:28 -0500

    add methods for modifying and deleting metadata keys

--- lib/modules/database/metadata.rb
+++ lib/modules/database/metadata.rb
@@ -12,6 +12,14 @@ module Powerbot
       def write(hash)
         update data: hash.to_json
       end
+
+      def merge(hash)
+        write read.merge(hash)
+      end
+
+      def delete(key)
+        write read.delete(key)
+      end
     end
 
     Metadata.unrestrict_primary_key
