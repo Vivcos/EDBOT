4eb0d40ea5fb1735022cc6a75064b21fd4a322ac
z64 <zachnowicki@gmail.com>
2017-01-13 21:08:15 -0500

    implement :push

--- lib/modules/commands/feeds.rb
+++ lib/modules/commands/feeds.rb
@@ -80,6 +80,39 @@ module Powerbot
 
       # Push content to a feed
       command(:push) do |event|
+        args = event.message.content[8..-1]
+                    .split('|')
+                    .map(&:strip)
+
+        next 'Not enough arguments. Format: `pal.push feed | title | content`' unless args.count == 3
+
+        name    = args.shift
+        title   = args.shift
+        content = args.shift
+
+        maybe_existing_feed = Database::Feed.find server_id: event.server.id, name: name
+        next 'Feed not found. Use `pal.feeds` for a list of feeds.' unless maybe_existing_feed
+        next "Title too long (#{content.length} / 100)" if title.length > 100
+        next 'Content too long' if content.length > 2048
+
+        role    = maybe_existing_feed.role
+        channel = maybe_existing_feed.channel
+
+        role.mentionable = true
+
+        channel.send_embed(
+          "🛰️ #{role.mention} **| #{title}**",
+          Discordrb::Webhooks::Embed.new(
+            description: content,
+            color: role.color.combined,
+            footer: { text: event.user.distinct, icon_url: event.user.avatar_url },
+            timestamp: Time.now
+          )
+        )
+
+        role.mentionable = false
+
+        '👌'
       end
     end
   end
