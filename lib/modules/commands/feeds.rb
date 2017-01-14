acef109d155712bee5f33b12cf6353988485b3a1
z64 <zachnowicki@gmail.com>
2017-01-13 20:05:19 -0500

    add placeholder feed command specs

---
+++ lib/modules/commands/feeds.rb
@@ -0,0 +1,31 @@
+module Powerbot
+  module DiscordCommands
+    module Feeds
+      extend Discordrb::Commands::CommandContainer
+
+      # List feeds
+      command(:feeds) do |event|
+      end
+
+      # Create feed
+      command(:create_feed) do |event|
+      end
+
+      # Delete a feed
+      command(:delete_feed) do |event|
+      end
+
+      # Subscribe to feed
+      command(:sub) do |event|
+      end
+
+      # Unsubscribe from feed
+      command(:unsub) do |event|
+      end
+
+      # Push content to a feed
+      command(:push) do |event|
+      end
+    end
+  end
+end
