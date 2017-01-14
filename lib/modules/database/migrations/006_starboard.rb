be5a672ecbe9fe783e2083964767e76aad2425ba
z64 <zachnowicki@gmail.com>
2017-01-14 16:43:04 -0500

    store starred message ids in StarMessage instead and only track users in Star

--- lib/modules/database/migrations/006_starboard.rb
+++ lib/modules/database/migrations/006_starboard.rb
@@ -2,15 +2,16 @@ Sequel.migration do
   up do
     create_table(:star_messages) do
       primary_key :id
-      Integer :channel_id, null: false
-      Integer :message_id, unique: true, null: false
-      Integer :star_message_id, unique: true, null: false
+      Integer :starred_channel_id
+      Integer :starred_message_id, unique: true, null: false
+      Integer :channel_id
+      Integer :message_id, unique: true
     end
 
     create_table(:stars) do
       primary_key :id
       Integer :user_id, null: false
-      foreign_key :star_message_id, :star_messages, on_delete: :cascade
+      foreign_key :star_message_id, on_delete: :cascade
     end
   end
 
