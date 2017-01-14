89c63a79097439bf194c1ca827c0a462ae3fbdf1
z64 <zachnowicki@gmail.com>
2017-01-13 19:59:09 -0500

    add methods to fetch server, role, channel from cache for a feed

--- lib/modules/database/migrations/feed.rb
+++ lib/modules/database/migrations/feed.rb
@@ -1,6 +1,17 @@
 module Powerbot
   module Database
     class Feed < Sequel::Model
+      def server
+        BOT.server server_id
+      end
+
+      def role
+        server.role role_id
+      end
+
+      def channel
+        server.channel channel_id
+      end
     end
-  end  
+  end
 end
