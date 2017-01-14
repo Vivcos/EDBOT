87c0237005819b6df8ed4757bd481b568c0e39f6
z64 <zachnowicki@gmail.com>
2017-01-13 19:57:43 -0500

    add server_id column

--- lib/modules/database/migrations/005_feeds.rb
+++ lib/modules/database/migrations/005_feeds.rb
@@ -2,8 +2,9 @@ Sequel.migration do
   up do
     create_table(:feeds) do
       primary_key :id
-      Integer :channel_id, null: false
+      Integer :server_id, null: false
       Integer :role_id, unique: true, null: false
+      Integer :channel_id, null: false
       String :name, null: false
     end
   end
