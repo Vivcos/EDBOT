49bce1a932f0d1c7c4770dbfbd08f3b873281bf2
z64 <zachnowicki@gmail.com>
2017-01-14 16:43:24 -0500

    add helpers to StarMessage

--- lib/modules/database/star_message.rb
+++ lib/modules/database/star_message.rb
@@ -2,6 +2,26 @@ module Powerbot
   module Database
     class StarMessage < Sequel::Model
       one_to_many :stars
+
+      def starred_message
+        BOT.channel(starred_channel_id, starred_message_id)
+      end
+
+      def message
+        BOT.channel(channel_id).message(message_id)
+      end
+
+      def rep
+        stars.count
+      end
+
+      def star_by(user_id)
+        stars.find { |s| s.user_id == user_id }
+      end
+
+      def starred_by?(user_id)
+        !!star_by(user_id)
+      end
     end
 
     class Star < Sequel::Model
