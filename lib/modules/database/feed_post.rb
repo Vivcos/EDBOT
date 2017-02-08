ec83eab7cd70822cd76534fc2ee4c515edaeb989
z64 <zachnowicki@gmail.com>
2017-02-08 00:54:02 -0500

    add a method to parse a FeedPost content into an embed

--- lib/modules/database/feed_post.rb
+++ lib/modules/database/feed_post.rb
@@ -10,6 +10,30 @@ module Powerbot
       def author
         BOT.user author_id
       end
+
+      def parse_content(include_title = false)
+        data = content.split '|'
+
+        range = include_title ? 1..-1 : 2..-1
+
+        fields = data[range].map do |f|
+          Discordrb::Webhooks::Field.new(
+            name: "\u200b",
+            value: f
+          )
+        end
+
+        Discordrb::Webhooks::Embed.new(
+          description: data.first,
+          fields: fields,
+          color: feed.role.color.combined,
+          footer: {
+            text: "#{author.distinct} [use 'pal.unsub #{feed.name}' to unsub]",
+            icon_url: author.avatar_url
+          },
+          timestamp: Time.now
+        )
+      end
     end
   end
 end
