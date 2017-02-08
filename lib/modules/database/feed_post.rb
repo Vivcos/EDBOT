fda2ad527443efe099e71dfb5154629db2d0f62b
z64 <zachnowicki@gmail.com>
2017-02-08 00:41:18 -0500

    add a method to load existing feed message

--- lib/modules/database/feed_post.rb
+++ lib/modules/database/feed_post.rb
@@ -2,6 +2,10 @@ module Powerbot
   module Database
     class FeedPost < Sequel::Model
       many_to_one :feed
+
+      def message
+        feed.channel.message message_id
+      end
     end
   end
 end
