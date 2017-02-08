f2c3d2e070ce51fd58b2e994e19a68d45a655f60
z64 <zachnowicki@gmail.com>
2017-02-08 00:36:30 -0500

    add other columns to feed_posts table

--- lib/modules/database/migrations/007_feed_posts.rb
+++ lib/modules/database/migrations/007_feed_posts.rb
@@ -2,7 +2,11 @@ Sequel.migration do
   up do
     create_table(:feed_posts) do
       primary_key :id
+      foreign_key :feed_id, :feeds, on_delete: :cascade
+      Integer :author_id
       Integer :message_id
+      String :title
+      String :content, length: 2000
     end
   end
 
